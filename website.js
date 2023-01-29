3224a747cff0fa31615f0502e5a7622ee3adb3fd
Filip Dimitrijevic <filipdi@kth.se>
2023-01-29 14:42:50 +0100

    update website.js

--- website.js
+++ website.js
@@ -1,59 +1,70 @@
-// Get all of the links in the navigation menu
-const links = document.querySelectorAll('nav a');
+import React, { useState, useEffect } from 'react';
 
-// Add a click event listener to each link
-links.forEach(link => {
-    link.addEventListener('click', e => {
-        // Prevent the default behavior of the link (redirecting to a new page)
+const Navigation = ({ links }) => {
+  const [activeLink, setActiveLink] = useState(null);
+
+  useEffect(() => {
+    links.forEach(link => {
+      link.addEventListener('click', e => {
         e.preventDefault();
-
-        // Get the section that the link points to
-        const section = document.querySelector(link.getAttribute('href'));
-
-        // Scroll to the section with smooth animation
-        section.scrollIntoView({
-            behavior: 'smooth'
-        });
-
-        // Add class active to the current link
-        links.forEach(link => link.classList.remove('active'));
-        link.classList.add('active');
+        const section = window.document.querySelector(link.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+        setActiveLink(link.getAttribute('href'));
+      });
     });
-});
 
-// Add active class to the current section when user scrolls
-const sections = document.querySelectorAll('main section');
-window.addEventListener('scroll', e => {
-    const currentPosition = window.scrollY + (window.innerHeight / 2);
-    sections.forEach(section => {
-        if (currentPosition > section.offsetTop && currentPosition < (section.offsetTop + section.offsetHeight)) {
-            links.forEach(link => {
-                if (link.getAttribute('href') === `#${section.id}`) {
-                    link.classList.add('active');
-                } else {
-                    link.classList.remove('active');
-                }
-            });
+    const sections = window.document.querySelectorAll('main section');
+    window.addEventListener('scroll', e => {
+      const currentPosition = window.scrollY + (window.innerHeight / 2);
+      sections.forEach(section => {
+        if (
+          currentPosition > section.offsetTop &&
+          currentPosition < (section.offsetTop + section.offsetHeight)
+        ) {
+          setActiveLink(`#${section.id}`);
         }
+      });
     });
-});
+  }, []);
 
-// Implement lazy loading of images
-const images = document.querySelectorAll('img[data-src]');
-const options = {
-    rootMargin: '0px 0px 50px 0px'
+  return (
+    <nav>
+      {links.map(link => {
+        const href = link.getAttribute('href');
+        return (
+          <a
+            key={href}
+            href={href}
+            className={href === activeLink ? 'active' : ''}
+          >
+            {link.textContent}
+          </a>
+        );
+      })}
+    </nav>
+  );
 };
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const img = entry.target;
-            img.setAttribute('src', img.getAttribute('data-src'));
-            observer.unobserve(img);
-        }
-    });
-}, options);
+const Image = ({ dataSrc }) => {
+  const [src, setSrc] = useState(null);
+  const options = { rootMargin: '0px 0px 50px 0px' };
 
-images.forEach(img => {
-    observer.observe(img);
-});
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setSrc(dataSrc);
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    const image = window.document.querySelector(`img[data-src="${dataSrc}"]`);
+    if (image) {
+      observer.observe(image);
+    }
+  }, [dataSrc]);
+
+  return <div><img src={src} data-src
+    ={dataSrc} alt=""/></div>;
+};
